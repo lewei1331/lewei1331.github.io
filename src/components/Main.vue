24fa9d430eb5e819c078458d90e3d928599b90aa
xilian <xilian@pixl.world>
2023-06-30 12:09:26 +0800

    ff

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+ 
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <menu-section></menu-section>
+</template>
+
+<script>
+Vue.component('menu-section', {
+	template: '<ul><li v-for="item in menuItems">{{ item.text }}</li></ul>',
+	data: function() {
+		return {
+			menuItems: [{
+			    text: 'About me'
+			}, {
+			    text: 'Articles'
+			}, {
+			    text: 'contact'
+			}]
+		};
+	}
+});
+</script>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
